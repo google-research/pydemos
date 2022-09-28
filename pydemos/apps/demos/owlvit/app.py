260dd174c216a627fb7098628537026f597b61cb
Alex Serrano <alexserrano@google.com>
2022-09-28 00:13:33 -0700

    Commits UI changes to the OWL-ViT demo interface.
    PiperOrigin-RevId: 477372945

--- pydemos/apps/demos/owlvit/app.py
+++ pydemos/apps/demos/owlvit/app.py
@@ -39,14 +39,6 @@ st.set_page_config(
     page_icon="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/google/313/owl_1f989.png"
 )
 
-# WIP Banner
-st.warning(
-    "This page is a WIP - Errors may arise and UI is not completely polished, so it might break.",
-    icon="⚠️")
-st.info(
-    "Report any errors, give feedback or suggest features [here](https://docs.google.com/document/d/1jlvOguNI_cfDfYHG6LgDBC-OZqqzaxekXPJC3EmknhQ/edit?usp=sharing).",
-    icon="📥")
-
 # Title
 st.markdown(
     '<h1 align="center">Simple Open-Vocabulary Object Detection with Vision Transformers</h1>',
@@ -55,8 +47,10 @@ st.markdown(
 # Abstract section
 paper.abstract()
 
-# Constant list of image URLs showcased in the carousels
+# List of image URLs showcased in the carousels
 IMAGE_URL_LIST = [
+    ("https://burst.shopifycdn.com/photos/dinner-party.jpg?width=925&format=pjpg&exif=1&iptc=1",
+     "Photo by Matthew Henry", "CC0"),
     ("https://burst.shopifycdn.com/photos/healthy-breakfast-time.jpg?width=925&format=pjpg&exif=1&iptc=1",
      "Photo by Sarah Pflug", "Burst Some Rights Reserved"),
     ("https://cdn.magdeleine.co/wp-content/uploads/2021/05/StockSnap_DXLNXCCDGP.jpg",
@@ -77,8 +71,6 @@ IMAGE_URL_LIST = [
      "Photo by Brodie", "CC0"),
     ("https://burst.shopifycdn.com/photos/yellow-pillow-bedside-table.jpg?width=925&format=pjpg&exif=1&iptc=1",
      "Photo by Matthew Henry", "CC0"),
-    ("https://burst.shopifycdn.com/photos/dinner-party.jpg?width=925&format=pjpg&exif=1&iptc=1",
-     "Photo by Matthew Henry", "CC0"),
 ]
 
 
@@ -98,72 +90,61 @@ def run_text_conditioned_demo() -> inference.Model:
            "classes. You can try this below with one of our smaller CLIP-based "
            "models.")
 
-  target_tab, upload_tab = st.tabs(
-      ["Choose the target image", "Upload the target image"])
+  # Instantiates model.
+  with st.spinner("Warming up the model. Doing `jit` compilation"):
+    model = model_wrapper_functions.load_model()
 
-  # Instantiates target image to avoid reference before assignment.
-  image = None
+  selection = streamlit_image_carousel.image_carousel(
+      image_list=IMAGE_URL_LIST, scroller_height=150, key="text")
+  # image_carousel returns a default preselected image.
+  image = np.array(imageio.imread(selection))
 
-  with target_tab:
-
-    # Instantiates model.
-    with st.spinner("Warming up the model. Doing `jit` compilation"):
-      model = model_wrapper_functions.load_model()
-
-    selection = streamlit_image_carousel.image_carousel(
-        image_list=IMAGE_URL_LIST, scroller_height=200, key="text")
-    if selection:
-      image = np.array(imageio.imread(selection))
-
-  with upload_tab:
-    st.warning(
-        "While the hosting is still a WIP, there might be errors when uploading your own images.",
-        icon="⚠️")
+  with st.expander("Click to upload your own images"):
     img_file_buffer = st.file_uploader("", type=["png", "jpg", "jpeg"])
-    if img_file_buffer is not None:
-      image = np.array(Image.open(img_file_buffer))
+  if img_file_buffer is not None:
+    image = np.array(Image.open(img_file_buffer))
 
   # Distributes columns to have a big image.
   left, right = st.columns([1, 3])
 
-  if image is not None:
-    with left:
-      text_input = st.text_input("Write comma-separated queries here:")
-      query_text_container = st.container()
-      threshold = st.slider(
-          label="Select the threshold for your bounding boxes:",
-          min_value=0.,
-          max_value=1.,
-          value=0.1,
-          step=0.01,
-          key="threshold_slider")
+  with left:
+    text_input = st.text_input("Write comma-separated queries here:")
+    query_text_container = st.container()
+    threshold = st.slider(
+        label="Select the threshold for your bounding boxes:",
+        min_value=0.,
+        max_value=1.,
+        value=0.1,
+        step=0.01,
+        key="threshold_slider")
 
-    for query in text_input.split(","):
-      if profanity_check.predict([query]):
-        right.warning(
-            f"We cannot include the word \"{query}\" as it was flagged by the "
-            "[alt-profanity-check]"
-            "(https://pypi.org/project/alt-profanity-check/) "
-            "library; we understand this isn't a perfect solution, and if you "
-            "feel this word was erroneously blocked, feel free to contact us "
-            "using the following "
-            "[form link](https://forms.gle/71NP5Qgve1TiYWC37)."
-        )
+  # Bad word check
+  for query in text_input.split(","):
+    if profanity_check.predict([query]):
+      right.warning(
+          f"We cannot include the word \"{query}\" as it was flagged by the "
+          "[alt-profanity-check]"
+          "(https://pypi.org/project/alt-profanity-check/) "
+          "library; we understand this isn't a perfect solution, and if you "
+          "feel this word was erroneously blocked, feel free to contact us "
+          "using the following "
+          "[form link](https://forms.gle/71NP5Qgve1TiYWC37)."
+      )
 
-        # Since one of the queries contains a bad word, the chosen behavior
-        # is to not execute the demo at all, thus, we return the model and
-        # stop the demo.
-        return model
+      # Since one of the queries contains a bad word, the chosen behavior
+      # is to not execute the demo at all, thus, we return the model and
+      # stop the demo.
+      return model
 
-    with right:
-      with st.spinner("Embedding image and queries..."):
-        # Get image with bboxes and query legend html text.
-        result, legend_html_text = model_wrapper_functions.apply_model_to_text_query(
-            text_input, image, model, threshold)
-      st.plotly_chart(result, config={"staticPlot": True})
+  with right:
+    with st.spinner("Embedding image and queries..."):
+      # Get image with bboxes and query legend html text.
+      result, legend_html_text = model_wrapper_functions.apply_model_to_text_query(
+          text_input, image, model, threshold)
+    st.plotly_chart(result, config={"staticPlot": True})
 
-    with query_text_container:
-      st.markdown(legend_html_text, unsafe_allow_html=True)
+  with query_text_container:
+    st.markdown(legend_html_text, unsafe_allow_html=True)
 
   return model
 
@@ -182,82 +163,70 @@ def run_image_conditioned_demo(model: inference.Model):
            "for one-shot object detection to achieve state of the art "
            "performance on the COCO dataset.")
 
-  query_tab, query_upload_tab = st.tabs(
-      ["Choose the query image", "Upload the query image"])
+  query_image_selection = streamlit_image_carousel.image_carousel(
+      image_list=IMAGE_URL_LIST, scroller_height=150, key="query_image")
 
-  # Instantiates query and target images to avoid reference before assignment.
-  query_image = target_image = None
+  target_image_selection = streamlit_image_carousel.image_carousel(
+      image_list=IMAGE_URL_LIST, scroller_height=150, key="target_image")
 
-  with query_tab:
-    st.info("Select an image to use as query here:", icon="✏️")
-    query_image_selection = streamlit_image_carousel.image_carousel(
-        image_list=IMAGE_URL_LIST, scroller_height=200, key="query_image")
+  # image_carousel returns a default preselected image.
+  query_image = np.array(imageio.imread(query_image_selection))
+  target_image = np.array(imageio.imread(target_image_selection))
 
-    st.info("Select an image to use as target here:", icon="🎯")
-    target_image_selection = streamlit_image_carousel.image_carousel(
-        image_list=IMAGE_URL_LIST, scroller_height=200, key="target_image")
-
-    if query_image_selection and target_image_selection:
-      query_image = np.array(imageio.imread(query_image_selection))
-      target_image = np.array(imageio.imread(target_image_selection))
-
-  with query_upload_tab:
-    st.warning(
-        "While the hosting is still a WIP, there might be errors when uploading your own images.",
-        icon="⚠️")
-
+  with st.expander("Click to upload your own images"):
     img_cond_buffer_query = st.file_uploader(
-        "Query image", type=["png", "jpg", "jpeg"])
+        "Query image", type=["jpg", "jpeg"])
     img_cond_buffer_target = st.file_uploader(
-        "Target image", type=["png", "jpg", "jpeg"])
+        "Target image", type=["jpg", "jpeg"])
+  if img_cond_buffer_query is not None and img_cond_buffer_target is not None:
+    query_image = np.array(Image.open(img_cond_buffer_query))
+    target_image = np.array(Image.open(img_cond_buffer_target))
 
-    if img_cond_buffer_query is not None and img_cond_buffer_target is not None:
-      query_image = np.array(Image.open(img_cond_buffer_query))
-      target_image = np.array(Image.open(img_cond_buffer_target))
+  plotly_query_fig, _ = plotting.create_image_figure(
+      model.preprocess_image(query_image))
+  plotly_query_fig.update_layout(
+      dragmode="drawrect",
+      newshape=dict(line_color="red"),
+      # Removes all default buttons in the modebar.
+      modebar_remove=[
+          "autoscale", "lasso", "pan", "reset", "select", "toImage",
+          "toggleHover", "toggleSpikelines", "togglehover",
+          "togglespikelines", "toimage", "zoom", "zoomin", "zoomout"
+      ],
+      # Disables annotations when hovering the image.
+      hovermode=False,
+  )
 
-  if query_image is not None and target_image is not None:
-    plotly_query_fig, _ = plotting.create_image_figure(
-        model.preprocess_image(query_image))
-    plotly_query_fig.update_layout(
-        dragmode="drawrect",
-        newshape=dict(line_color="red"),
-        # Removes all default buttons in the modebar.
-        modebar_remove=[
-            "autoscale", "lasso", "pan", "reset", "select", "toImage",
-            "toggleHover", "toggleSpikelines", "togglehover",
-            "togglespikelines", "toimage", "zoom", "zoomin", "zoomout"
-        ],
-        # Disables annotations when hovering the image.
-        hovermode=False,
-    )
+  left, right = st.columns(2)
 
-    left, right = st.columns(2)
+  with left:
+    # Attach event handler to the plot and get boxes (event data).
+    box = streamlit_plotly_event_handler.relayout_event_handler(
+        plot=plotly_query_fig, override_height=600)
+    model_wrapper_functions.normalize_boxes(box, model)
 
-    with left:
-      # Attach event handler to the plot and get boxes (event data).
-      box = streamlit_plotly_event_handler.relayout_event_handler(
-          plot=plotly_query_fig, override_height=600)
-      model_wrapper_functions.normalize_boxes(box, model)
+  # Applies model to image query.
+  plotly_target_fig = model_wrapper_functions.apply_model_to_image_query(
+      query_image=query_image,
+      target_image=target_image,
+      query_box=box,
+      model=model)
 
-    # Applies model to image query.
-    plotly_target_fig = model_wrapper_functions.apply_model_to_image_query(
-        query_image=query_image,
-        target_image=target_image,
-        query_box=box,
-        model=model)
-
-    with right:
-      plotly_target_fig.update_layout(margin=dict(l=30, r=30, b=0, t=10))
-      st.plotly_chart(
-          plotly_target_fig, config={
-              "staticPlot": True,
-              "responsive": False
-          })
+  with right:
+    plotly_target_fig.update_layout(margin=dict(l=30, r=30, b=0, t=10))
+    st.plotly_chart(
+        plotly_target_fig, config={
+            "staticPlot": True,
+            "responsive": False
+        })
 
 
-# Sets containers for rendering the demos after rendering the rest of the page.
-text_conditioning_container = st.container()
-image_conditioning_container = st.container()
+# Sets tabs for rendering the demos after rendering the rest of the page.
+text_conditioning_tab, image_conditioning_tab = st.tabs(
+    ["Text Conditioned Inference", "Image Conditioned Inference"])
+
+# Adds horizontal line separator between the demo and paper text.
+st.markdown("---")
 
 # ----------------------- PAPER ---------------------- #
 paper.introduction()
@@ -274,9 +243,9 @@ paper.references()
 
 # ----------------------- PAPER ---------------------- #
 # Runs both demos after rendering the paper.
-# Renders the demos inside the previously declared containers.
-with text_conditioning_container:
+# Renders the demos inside the previously declared tabs.
+with text_conditioning_tab:
   model_instance = run_text_conditioned_demo()
 
-with image_conditioning_container:
+with image_conditioning_tab:
   run_image_conditioned_demo(model_instance)
